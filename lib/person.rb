247be277eda34af336adb03c49c1783adcd43a3d
lamass <hadzy@yahoo.com>
2015-12-11 05:45:41 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+
+class Person
+  def name=(some_name)
+    @name = some_name
+  end
+  def name
+    @name
+  end
+  def job=(some_job)
+    @job = some_job
+  end
+  def job
+    @job
+  end
+end
